fa274e92fb547f2c321f22e14bed5c93b9089b72
abdullahnuby <138891293+abdullahnuby@users.noreply.github.com>
2026-09-02 11:14:32 +0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma/client'
 import { PrismaD1 } from '@prisma/adapter-d1'
 import { env as cloudflareEnv } from 'cloudflare:workers'
 
